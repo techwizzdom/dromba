5206f6cbd2f42238f713c93951a8681acb741bfc
drombs <domagoj.vidovic@infinum.hr>
2020-04-20 17:52:07 +0200

    Style theme toggle

--- src/components/ThemeToggle/index.tsx
+++ src/components/ThemeToggle/index.tsx
@@ -6,19 +6,42 @@ interface IProps {
   toggleTheme: any;
 }
 
-const togglerContainer = (theme: any) => css`
-  border-radius: 20px;
+const togglerContainerCss = (theme: any) => css`
   padding: 2px;
-  border: 1px solid ${theme.border};
+  border: none;
+  background-color: ${theme.backgroundColor};
+  transition-property: background-color;
+  transition-duration: 0.5s;
   height: 26px;
-  width: 50px;
+  width: 56px;
+  outline: none;
+  position: relative;
+  cursor: pointer;
 `;
 
-const switcher = (theme: any) => css`
+const switcherCss = (theme: any) => css`
   border-radius: 50px;
   width: 20px;
   height: 20px;
-  background-color: ${theme.border};
+  background-color: ${theme.themeToggleColor};
+  margin-left: ${theme.isDark ? '30px' : '0'};
+  transition-property: margin, background-color;
+  transition-duration: 0.5s;
+  transition-timing-function: ease;
+  position: absolute;
+  z-index: 1;
+  top: 4px;
+`;
+
+const switcherLineCss = (theme: any) => css`
+  position: absolute;
+  z-index: 0;
+  width: 36px;
+  height: 8px;
+  top: 10px;
+  left: 10px;
+  border-radius: 20px;
+  background-color: ${theme.themeToggleLine};
 `;
 
 const ThemeToggle: React.FC<IProps> = (props: IProps) => {
@@ -26,10 +49,11 @@ const ThemeToggle: React.FC<IProps> = (props: IProps) => {
   return (
     <button
       type="button"
-      className={togglerContainer(theme)}
+      className={togglerContainerCss(theme)}
       onClick={() => props.toggleTheme()}
     >
-      <div className={switcher(theme)} />
+      <div className={switcherCss(theme)} />
+      <div className={switcherLineCss(theme)} />
     </button>
   );
 };
