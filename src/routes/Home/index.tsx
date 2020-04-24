32f011d4e8cf3442bfa276c7f9e7dae80fbc97e2
drombs <domagoj.vidovic@infinum.hr>
2020-04-24 17:51:12 +0200

    Add text to home route

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import RouteContainer from '../../components/RouteContainer';
+import TextSection from '../../components/TextSection';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <RouteContainer>
+      <TextSection
+        title="Random title"
+        content="Curabitur eget eros eros. Mauris laoreet justo vel facilisis fermentum. Aliquam venenatis pellentesque elit, at ornare ex tempor et. Fusce molestie sagittis bibendum. Quisque nec purus id urna volutpat rhoncus. Cras sit amet nulla ligula. Fusce maximus sed leo eu condimentum. Curabitur id dui id tortor consectetur mattis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur sit amet tincidunt libero. Fusce maximus lectus."
+      />
+    </RouteContainer>
+  );
 }
 
 export default Home;
