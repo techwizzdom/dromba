6319c133e5efc1f429fc8c62201c0b6321ec9274
drombs <domagoj.vidovic@infinum.hr>
2020-04-26 15:46:25 +0200

    Create hook for fecthing medium blog posts

---
+++ src/services/useMediumBlogPosts.tsx
@@ -0,0 +1,38 @@
+import { useFetch } from '../hooks/useFetch';
+
+export const useMediumBlogPosts = () => {
+  const { data, isLoading } = useFetch(
+    'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@dvidovic91',
+  );
+
+  if (!isLoading) {
+    return [
+      data.items
+        .filter((item: any) => item.categories.length)
+        .map((item: any) => {
+          return {
+            title: item.title,
+            subtitle: blogPostSubtitle(item),
+            thumbnail: blogPostThumbnailSmall(item),
+            url: item.link,
+          };
+        }),
+      isLoading,
+    ];
+  }
+
+  return [null, isLoading];
+};
+
+const blogPostSubtitle = (blogPostItem: any) => {
+  return blogPostItem.description
+    .split('medium-feed-snippet')[1]
+    .split('>')[1]
+    .split('<')[0];
+};
+
+const blogPostThumbnailSmall = (blogPostItem: any) => {
+  const imageLarge = blogPostItem.description.split('src')[1].split('"')[1];
+
+  return imageLarge.replace('max/2600', 'fit/c/304/312');
+};
