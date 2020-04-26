62ac00c1f29b75e83afa73c9ea3cb4ede7641455
drombs <domagoj.vidovic@infinum.hr>
2020-04-26 15:45:16 +0200

    Add heading components

--- src/components/core/Heading/index.tsx
+++ src/components/core/Heading/index.tsx
@@ -6,16 +6,55 @@ import { HeadingSize } from '../../../enums/HeadingSize';
 interface IHeadingProps {
   children: React.ReactNode;
   tag: HeadingTags;
+  size: HeadingSize;
 }
 
-const headingCss = (tag: HeadingTags) => css`
-  font-size: 32px;
+interface IHProps {
+  children: React.ReactNode;
+}
+
+const headingCss = (size: HeadingSize) => css`
+  font-size: ${size}px;
 `;
 
 const Heading: React.FC<IHeadingProps> = (props: IHeadingProps) => {
-  const { tag, children } = props;
+  const { tag, size, children } = props;
 
-  return React.createElement(tag, { className: headingCss(tag) }, children);
+  return React.createElement(tag, { className: headingCss(size) }, children);
 };
 
-export default Heading;
+export const H1: React.FC<IHProps> = (props: IHProps) => (
+  <Heading tag={HeadingTags.H1} size={HeadingSize.H1}>
+    {props.children}
+  </Heading>
+);
+
+export const H2: React.FC<IHProps> = (props: IHProps) => (
+  <Heading tag={HeadingTags.H2} size={HeadingSize.H2}>
+    {props.children}
+  </Heading>
+);
+
+export const H3: React.FC<IHProps> = (props: IHProps) => (
+  <Heading tag={HeadingTags.H3} size={HeadingSize.H3}>
+    {props.children}
+  </Heading>
+);
+
+export const H4: React.FC<IHProps> = (props: IHProps) => (
+  <Heading tag={HeadingTags.H4} size={HeadingSize.H4}>
+    {props.children}
+  </Heading>
+);
+
+export const H5: React.FC<IHProps> = (props: IHProps) => (
+  <Heading tag={HeadingTags.H5} size={HeadingSize.H5}>
+    {props.children}
+  </Heading>
+);
+
+export const H6: React.FC<IHProps> = (props: IHProps) => (
+  <Heading tag={HeadingTags.H6} size={HeadingSize.H6}>
+    {props.children}
+  </Heading>
+);
