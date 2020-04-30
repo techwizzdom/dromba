c533ad3fc671cc21f9b2a43d9b223c9438de233a
drombs <domagoj.vidovic@infinum.hr>
2020-04-30 20:51:06 +0200

    Update header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,45 +1,26 @@
 import * as React from 'react';
-import { css } from 'emotion';
-import ThemeToggle from '../ThemeToggle';
-import MainNavigation from '../MainNavigation';
-import Logo from '../../assets/icons/logo.svg';
+import { useDevice } from '../../hooks/useDevice';
+import { DeviceType } from '../../enums/DeviceType';
+import HeaderMobile from '../HeaderMobile';
+import HeaderDesktop from '../HeaderDesktop';
 
 interface IHeaderProps {
   toggleTheme: () => void;
 }
 
-const headerContainerCss = css`
-  height: 48px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const logoWrapperCss = css`
-  padding: 0 16px;
-`;
-
-const logoCss = css`
-  width: 56px;
-  min-width: 56px;
-  height: 40px;
-`;
-
-const themeToggleContainerCss = css`
-  padding: 0 16px;
-`;
-
 const Header: React.FC<IHeaderProps> = (props: IHeaderProps) => {
+  const { toggleTheme } = props;
+
+  const deviceType = useDevice();
+
   return (
-    <div className={headerContainerCss}>
-      <div className={logoWrapperCss}>
-        <img src={Logo} className={logoCss} alt="logo" />
-      </div>
-      <MainNavigation />
-      <div className={themeToggleContainerCss}>
-        <ThemeToggle toggleTheme={props.toggleTheme} />
-      </div>
-    </div>
+    <>
+      {deviceType === DeviceType.Mobile ? (
+        <HeaderMobile toggleTheme={toggleTheme} />
+      ) : (
+        <HeaderDesktop toggleTheme={toggleTheme} />
+      )}
+    </>
   );
 };
 
