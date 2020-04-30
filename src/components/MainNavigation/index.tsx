32df02b8bfe0b61e45fcffdeea7dabc43598baff
drombs <domagoj.vidovic@infinum.hr>
2020-04-30 20:53:42 +0200

    Update main navigation

--- src/components/MainNavigation/index.tsx
+++ src/components/MainNavigation/index.tsx
@@ -1,10 +1,7 @@
 import * as React from 'react';
 import { css } from 'emotion';
-import { ThemeContext } from '../ThemeContext';
-import { Link } from 'react-router-dom';
-import { TextHoverUnderlineHeight } from '../../enums/TextHoverUnderlineHeight';
-import { underlineOnHover } from '../../styles/css/textHover';
 import { MediaBreakpoints } from '../../enums/MediaBreakpoints';
+import NavigationItems from '../NavigationItems';
 
 const mainNavigationCss = css`
   font-family: 'Indie Flower', cursive;
@@ -22,29 +19,10 @@ const mainNavigationCss = css`
   }
 `;
 
-const navigationItemCss = (theme: any) => css`
-  position: relative;
-  color: ${theme.textColor};
-
-  ${underlineOnHover(TextHoverUnderlineHeight.Medium, theme)}
-`;
-
 const MainNavigation: React.FC = () => {
-  const theme = React.useContext(ThemeContext);
   return (
     <nav className={mainNavigationCss}>
-      <Link to="/" className={navigationItemCss(theme)}>
-        Das Haus
-      </Link>
-      <Link to="timeline" className={navigationItemCss(theme)}>
-        Timeline
-      </Link>
-      <Link to="about" className={navigationItemCss(theme)}>
-        About who?
-      </Link>
-      <Link to="blog" className={navigationItemCss(theme)}>
-        Blog
-      </Link>
+      <NavigationItems />
     </nav>
   );
 };
