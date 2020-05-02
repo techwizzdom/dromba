f05e143268a3d4bc69fa89e1c79a5ed8bc81b540
drombs <domagoj.vidovic@infinum.hr>
2020-05-02 14:50:24 +0200

    Underline navigation item if selected

--- src/components/NavigationItems/index.tsx
+++ src/components/NavigationItems/index.tsx
@@ -1,42 +1,74 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
-import { underlineOnHover } from '../../styles/css/textHover';
+import { Link, useLocation } from 'react-router-dom';
+import { underlineOnHover, underline } from '../../styles/css/textHover';
 import { css } from 'emotion';
 import { ThemeContext } from '../ThemeContext';
+import { Routes } from '../../routes/Routes';
 
 interface INavigationItemsProps {
+  onNavigationItemClick?: () => void;
   isMobile?: boolean;
 }
 
-const navigationItemCss = (theme: any, isMobile: boolean) => css`
+const navigationItemCss = (
+  theme: any,
+  isMobile: boolean,
+  isSelected: boolean,
+) => css`
   position: relative;
   color: ${theme.textColor};
   display: ${isMobile ? 'flex' : 'block'};
   justify-content: center;
-  height: ${isMobile ? '32px' : 'auto'};
+  margin: ${isMobile ? '8px 0' : '0'};
 
   ${underlineOnHover(theme)};
+  ${isSelected ? underline(theme) : null}
 `;
 
 const NavigationItems: React.FC<INavigationItemsProps> = (
   props: INavigationItemsProps,
 ) => {
-  const { isMobile = false } = props;
+  const { onNavigationItemClick, isMobile = false } = props;
 
+  const location = useLocation();
   const theme = React.useContext(ThemeContext);
+
+  const isSelected = (route: Routes): boolean => {
+    return route === location.pathname;
+  };
 
   return (
     <>
-      <Link to="/" className={navigationItemCss(theme, isMobile)}>
+      <Link
+        to={Routes.Home}
+        className={navigationItemCss(theme, isMobile, isSelected(Routes.Home))}
+        onClick={() => onNavigationItemClick && onNavigationItemClick()}
+      >
         Das Haus
       </Link>
-      <Link to="timeline" className={navigationItemCss(theme, isMobile)}>
+      <Link
+        to={Routes.Timeline}
+        className={navigationItemCss(
+          theme,
+          isMobile,
+          isSelected(Routes.Timeline),
+        )}
+        onClick={() => onNavigationItemClick && onNavigationItemClick()}
+      >
         Timeline
       </Link>
-      <Link to="about" className={navigationItemCss(theme, isMobile)}>
+      <Link
+        to={Routes.About}
+        className={navigationItemCss(theme, isMobile, isSelected(Routes.About))}
+        onClick={() => onNavigationItemClick && onNavigationItemClick()}
+      >
         About who?
       </Link>
-      <Link to="blog" className={navigationItemCss(theme, isMobile)}>
+      <Link
+        to={Routes.Blog}
+        className={navigationItemCss(theme, isMobile, isSelected(Routes.Blog))}
+        onClick={() => onNavigationItemClick && onNavigationItemClick()}
+      >
         Blog
       </Link>
     </>
