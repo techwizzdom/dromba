e181c4101154b196dd4ebbf3b95b6963d8932d83
dom-patrick <domagoj.vidovic@productive.io>
2020-08-10 20:54:36 +0200

    Update content

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import RouteContainer from '../../components/RouteContainer';
-import { H1 } from '../../components/core/Heading';
+import { H1, H6 } from '../../components/core/Heading';
 import TimelineCard from '../../components/TimelineCard';
 import IndentContainer from '../../components/IndentContainer';
 import VerticalSpacing from '../../components/VerticalSpacing';
@@ -16,21 +16,53 @@ function Home() {
 
       <VerticalSpacing height={VerticalSpacingHeight.Giant} />
 
+      <H6>
+        As a 25 years old guy, there's a lot for me to achieve. Many things I
+        don't know; zero of them I can't learn. Nothing beats consistent effort,
+        paired with elevated motivation and intention.
+      </H6>
+
+      <VerticalSpacing height={VerticalSpacingHeight.Medium} />
+
+      <H6>
+        In the early days of my Computer Science studies, I've landed my first
+        intership. The rest is history; I'm still working, craving for endless
+        opportunities. Here's a short summary of my working experience:
+      </H6>
+
+      <VerticalSpacing height={VerticalSpacingHeight.Xlarge} />
+
       <IndentContainer>
         <TimelineCard
           title="Productive"
           subtitle="Aug 2019 - still here"
-          description="Curabitur eget eros eros. Mauris laoreet justo vel facilisis fermentum. Aliquam venenatis pellentesque elit, at ornare ex tempor et. Fusce molestie sagittis bibendum. Quisque nec purus id urna volutpat rhoncus. Cras sit amet nulla ligula. Fusce maximus sed leo eu condimentum. Curabitur id dui id tortor consectetur mattis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur sit amet tincidunt libero. Fusce maximus lectus."
+          upperDescription="One of the most successful agencies in Croatia, Infinum, didn't like any of available tools for managing their business. That's why they started to build their own product - Productive, which is growing since 2015."
+          lowerDescription="Besides the 'regular engineering stuff' (Frontend, Backend, Design), working on a product like this brought me massive knowledge in the fields of Business Management, Product Ownership, and Sales."
         />
+
         <TimelineCard
           title="Infinum"
           subtitle="Feb 2019 - still here"
-          description="Curabitur eget eros eros. Mauris laoreet justo vel facilisis fermentum. Aliquam venenatis pellentesque elit, at ornare ex tempor et. Fusce molestie sagittis bibendum. Quisque nec purus id urna volutpat rhoncus. Cras sit amet nulla ligula. Fusce maximus sed leo eu condimentum. Curabitur id dui id tortor consectetur mattis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur sit amet tincidunt libero. Fusce maximus lectus."
+          upperDescription="Before Productive, I worked with the US-based clients on {{Loop Community}}; an audio project where Artists can upload their songs divided into basic parts, like voices and instruments."
+          lowerDescription="My main tasks were building beautiful user interfaces, as well as close communication with Design and Backend teams to make it done."
         />
+
+        <TimelineCard
+          title="OptimIT"
+          subtitle="Feb 2018 - Feb 2019"
+          upperDescription="I've worked as a full-stack developer on E-commerce projects for two big Telecommunication companies in region - {{A1}} and {{Tomato}}."
+        />
+
         <TimelineCard
           title="Asseco SEE"
           subtitle="Jan 2017 - July 2017"
-          description="Curabitur eget eros eros. Mauris laoreet justo vel facilisis fermentum. Aliquam venenatis pellentesque elit, at ornare ex tempor et. Fusce molestie sagittis bibendum. Quisque nec purus id urna volutpat rhoncus. Cras sit amet nulla ligula. Fusce maximus sed leo eu condimentum. Curabitur id dui id tortor consectetur mattis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur sit amet tincidunt libero. Fusce maximus lectus."
+          upperDescription="At first, I was working on an internal file sharing platform; later on, I got my hands onto the {{PBZ mobile banking app}}, one of the largest banks in Croatia under the Intesa Sanpaolo bank."
+        />
+
+        <TimelineCard
+          title="OptimIT"
+          subtitle="Jul 2015 - Aug 2015"
+          upperDescription="This was my first and last internship, where I used to work on a project for creating digital signature on PDF documents."
         />
       </IndentContainer>
     </RouteContainer>
