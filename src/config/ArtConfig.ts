56c025d4a1128a4d161e4fd9d11da6891ae92d24
dom-patrick <domagoj.vidovic@productive.io>
2020-09-05 21:13:04 +0200

    Extend art config

--- src/config/ArtConfig.ts
+++ src/config/ArtConfig.ts
@@ -1,7 +1,20 @@
+import { MediaBreakpoints } from '../enums/MediaBreakpoints';
+import { DeviceType } from '../enums/DeviceType';
+
+const getDeviceType = () => {
+  const width = window.innerWidth;
+
+  if (width <= MediaBreakpoints.Tablet && width > MediaBreakpoints.Mobile) {
+    return DeviceType.Tablet;
+  } else if (width <= MediaBreakpoints.Mobile) {
+    return DeviceType.Mobile;
+  }
+};
+
 export const ArtConfig = {
   numberOfLines: 300,
-  colorOpacity: 0.5,
+  colorOpacity: 0.8,
   canvasOpacityMin: 0,
   canvasOpacityMax: 0.8,
-  duration: 6000,
+  duration: getDeviceType() === DeviceType.Mobile ? 6500 : 6000,
 };
