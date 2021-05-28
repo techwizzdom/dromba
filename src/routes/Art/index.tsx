fe670be3e88b5f2688c4f07c4cf96ed2f70323c6
Domagoj Vidovic <domagoj@orbitalwitness.com>
2021-05-28 08:20:53 +0100

    Fix diving issues on Android

--- src/routes/Art/index.tsx
+++ src/routes/Art/index.tsx
@@ -83,10 +83,6 @@ function Art(props: IArtProps) {
         }),
       2100,
     );
-    setTimeout(() => {
-      setIsArtEnabled(true);
-      setArtStartTime(Date.now());
-    }, 2200);
     setTimeout(
       () =>
         setIsLogoDiving({
@@ -94,8 +90,12 @@ function Art(props: IArtProps) {
           isJumping: false,
           isOriginalVisible: true,
         }),
-      2500,
+      3000,
     );
+    setTimeout(() => {
+      setIsArtEnabled(true);
+      setArtStartTime(Date.now());
+    }, 2250);
   };
 
   return (
@@ -105,6 +105,11 @@ function Art(props: IArtProps) {
           isRandomArtEnabled={true}
           onClick={() => {
             setIsArtEnabled(false);
+            setIsLogoDiving({
+              isMoving: false,
+              isJumping: false,
+              isOriginalVisible: true,
+            });
             trackEvent(
               'Art Close',
               'Random art closed',
@@ -126,6 +131,8 @@ function Art(props: IArtProps) {
             className={artButtonCss(theme)}
             onClick={() => {
               launchDiving();
+              // setIsArtEnabled(true);
+              // setArtStartTime(Date.now());
             }}
           >
             {t.art.actionButton}
