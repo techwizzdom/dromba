98dc0440ac0e7318447ae022f82f80a944ce02b3
Domagoj Vidovic <domagoj@orbitalwitness.com>
2021-11-29 16:40:59 +0000

    Update helmetiser

--- src/components/core/Helmetiser/index.tsx
+++ src/components/core/Helmetiser/index.tsx
@@ -6,39 +6,25 @@ interface IHelmetiserProps {
   image?: string;
 }
 
-const DEFAULT_TITLE = 'Personal Website And Blog';
-const DEFAULT_DESCRIPTION =
-  'Frontend Engineer, mentor, and speaker in a London tech startup. Sharing my thoughts and ideas.';
-const DEFAULT_IMAGE = 'https://i.imgur.com/63zrOmd.png';
-
 function Helmetiser(props: IHelmetiserProps) {
-  const { title, description, image } = props;
-
-  const updatedTitle = `${title || DEFAULT_TITLE} — Domagoj Vidovic`;
-  document.title = updatedTitle;
+  const { title, description } = props;
+  document.title = `${title} — Domagoj Vidovic`;
   document
     .querySelector('[property="og:title"]')
-    ?.setAttribute('content', updatedTitle);
+    ?.setAttribute('content', title || '');
   document
     .querySelector('[name="twitter:title"]')
-    ?.setAttribute('content', updatedTitle);
+    ?.setAttribute('content', title || '');
 
   document
     .querySelector('[name="description"]')
-    ?.setAttribute('content', description || DEFAULT_DESCRIPTION);
+    ?.setAttribute('content', description || '');
   document
     .querySelector('[property="og:description"]')
-    ?.setAttribute('content', description || DEFAULT_DESCRIPTION);
+    ?.setAttribute('content', description || '');
   document
     .querySelector('[name="twitter:description"]')
-    ?.setAttribute('content', description || DEFAULT_DESCRIPTION);
-
-  document
-    .querySelector('[property="og:image"]')
-    ?.setAttribute('content', image || DEFAULT_IMAGE);
-  document
-    .querySelector('[name="twitter:image"]')
-    ?.setAttribute('content', image || DEFAULT_IMAGE);
+    ?.setAttribute('content', description || '');
 
   return <></>;
 }
