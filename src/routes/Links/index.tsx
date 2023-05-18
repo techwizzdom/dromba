7fa4e81bfe01a1b258903527bf968308b745c794
dromba <dvidovic91@gmail.com>
2023-05-18 17:56:23 +0200

    Update link

--- src/routes/Links/index.tsx
+++ src/routes/Links/index.tsx
@@ -54,6 +54,15 @@ function Links() {
         <H5 alignCenter={true}>
           <Hyperlink
             isUnderline={true}
+            onClick={() => trackEvent('10Web', 'Click')}
+            href={t.link.tenWeb}
+          >
+            10Web (Website generator)
+          </Hyperlink>
+        </H5>
+        <H5 alignCenter={true}>
+          <Hyperlink
+            isUnderline={true}
             onClick={() => trackEvent('Scribe', 'Click')}
             href={t.link.scribe}
           >
