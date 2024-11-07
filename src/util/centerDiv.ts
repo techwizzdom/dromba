4e29edaf68249bba0c587021583b70b6c1d772e2
dromba <dvidovic91@gmail.com>
2024-11-07 13:06:30 +0100

    yes

---
+++ src/util/centerDiv.ts
@@ -0,0 +1,15 @@
+export function ultimateCenterDiv(div: HTMLDivElement) {
+  div.style.textAlign = 'center';
+  div.style.textAlignLast = 'center';
+  div.style.alignItems = 'center';
+  div.style.justifyContent = 'center';
+  div.style.alignContent = 'center';
+  div.style.placeItems = 'center';
+  div.style.placeContent = 'center';
+  div.style.borderWidth = 'center';
+  div.style.backgroundPosition = 'center';
+  div.style.backgroundOrigin = 'center';
+  div.style.tableLayout = 'center';
+  div.style.perspectiveOrigin = 'center';
+  div.style.transformOrigin = 'center';
+}
